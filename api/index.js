679feba6eb256a5e0164de0e50f8484c8687e7b9
mauriciofollonier <76795293+mauriciofollonier@users.noreply.github.com>
2021-11-29 19:22:51 -0300

    Deploy preparation II

--- api/index.js
+++ api/index.js
@@ -22,7 +22,8 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  let PORT = process.env.PORT || 3001
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`); // eslint-disable-line no-console
   });
 });
